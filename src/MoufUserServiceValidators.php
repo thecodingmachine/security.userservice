ab01169f27a104629d9ac8e5f749e5566e1cbf90
David Négrier <david@davidtcm>
2012-11-24 13:17:30 +0100

    Fixing validator

--- src/MoufUserServiceValidators.php
+++ src/MoufUserServiceValidators.php
@@ -5,5 +5,12 @@
 * See the file LICENSE.txt for copying permission.
 */
 
-MoufAdmin::getValidatorService()->registerBasicValidator('UserService validator', 'plugins/security/userservice/1.0/direct/userservice_instance_validator.php', array("selfedit"));
+// FIXME: we should not call $_REQUEST. We need a static Context object that we can call instead and that
+// contains the environment we are working on (config, DI container, etc...)
+if (isset($_REQUEST['selfedit']) && $_REQUEST['selfedit']=="true") {
+	$url = 'vendor/mouf/security.userservice/src/direct/userservice_instance_validator.php';
+} else {
+	$url = '../../../vendor/mouf/security.userservice/src/direct/userservice_instance_validator.php';
+}
+MoufAdmin::getValidatorService()->registerBasicValidator('UserService validator', $url, array("selfedit"));
 ?>
